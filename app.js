285551d5349cc9729f3e44ccb921d0be78df7284
Sure-Plus <xfreell@163.com>
2021-08-21 22:11:37 +0800

    实现登录功能

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ App({
   onLaunch() {
     // 初始化云开发环境
     wx.cloud.init({
-      env: 'aigroup-2gtja1ym4715274c' //云开发环境id
+        env: 'aigroup-2gtja1ym4715274c' //云开发环境id
     })
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    // 获取openid写入缓存
+    wx.cloud.callFunction({
+      name: "login",
+      success(res) {
+        // console.log(res.result.openid)
+        wx.setStorageSync('openid', res.result.openid)
       }
     })
   },
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,31 @@ const app = getApp()
 
 Page({
   data: {
-    
+    userInfo: null
   },
-  // 事件处理函数
+  onLoad() {
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    })
+  },
+  //登录
+  Login() {
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        // console.log(res.userInfo)
+        wx.setStorageSync('userInfo', res.userInfo)
+        this.setData({
+          userInfo: res.userInfo
+        })
+      }
+    })
+  },
+  //注销
+  Logout() {
+    wx.setStorageSync('userInfo', null)
+    this.setData({
+      userInfo: null
+    })
+  }
 })
