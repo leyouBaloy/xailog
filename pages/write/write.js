8568fdd49dba468d948f1fb79dc0988567f2cd1d
leyouBaloy <71641224+leyouBaloy@users.noreply.github.com>
2021-08-22 23:02:34 +0800

    实现写入数据库时间，日志内容
    附件上传暂未实现
    数据库中的时间上传的是时间戳，自动转化

--- pages/write/write.js
+++ pages/write/write.js
@@ -1,9 +1,12 @@
 Page({
   data: {
-    date: "",
+    //日历
     show: false,
-    value: "",
+    date: "",
+    datestamp:1,
+    //日志内容
     content: "",
+    //附件
     fileList: [],
   },
 
@@ -13,22 +16,39 @@ Page({
   onClose() {
     this.setData({ show: false });
   },
-  formatDate(date) {
-    date = new Date(date);
-    return `${date.getMonth() + 1}/${date.getDate()}`;
-  },
   onConfirm(event) {
+    let date = new Date(event.detail);
+    let datestr = `${date.getMonth() + 1}月${date.getDate()}日`
+    let datestamp = Date.parse(date) //化成时间戳存入数据库
     this.setData({
       show: false,
-      date: this.formatDate(event.detail),
+      date: datestr,
+      datestamp: datestamp,
     });
   },
-  onChange(event) {
-    // event.detail 为当前输入的值
-    console.log(event.detail);
-  },
+  
+    
   contentOnChange(event) {
     // event.detail 为当前输入的值
-    console.log(event.detail);
+    // console.log(event.detail);
+    this.setData({
+      content: event.detail
+    })
   },
-});
+
+  submit() {
+    wx.cloud.database().collection("logs")
+    .add({//提交数据到数据库
+      data: {
+        time: this.data.date,
+        content: this.data.content
+      }
+    })
+    .then(res => {
+      console.log("添加成功", res)
+    })
+    .catch(res => {
+      console.log("添加失败", res)
+    })
+  }
+})
