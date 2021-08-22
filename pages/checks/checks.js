b3098309ad26c90ff6f95a51e4610f91a6d361e7
Ashleywwj <2201639978@qq.com>
2021-08-22 11:29:46 +0800

    奖励小昕星少点了一个文件

--- pages/checks/checks.js
+++ pages/checks/checks.js
@@ -1,53 +1,59 @@
 // pages/checks/checks.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
     data: {
         list:[],
         userInfo: null,
-        user_id:''
+        isstar:''
       },
     
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function (options) {
         this.setData({
             userInfo: wx.getStorageSync('userInfo'),
-            user_id:'cd045e756120b7b406f45cec4050f728'
           })
         var user_id='cd045e756120b7b406f45cec4050f728'
         // var user_time=options.user_time
         wx.cloud.database().collection('logs')
-        .doc(user_id
-            // id=id
-        )
+        .doc(user_id)
         .get()
         .then(res =>{
             console.log('返回的数据',res.data)
             this.setData({
-                list:res.data
+                list:res.data,
+                isstar:res.data.ifstar
             })
-        }
-            )
+        })
 
     },
-    good(){
+    good:function(){
     var user_id='cd045e756120b7b406f45cec4050f728'
-    wx.cloud.database().collection('logs')
-    .doc(user_id
-    )
-    .update(
-        {
-            data:{
-                ifstar:true
-            }
-        }
-    )
+    wx.cloud.database().collection('logs').doc(user_id)
+    .get()
     .then(res =>{
-        console.log('返回的数据',res)
+        if (res.data.ifstar==true){
+            wx.cloud.database().collection('logs').doc(user_id).update({
+                // data 传入需要局部更新的数据
+                data: {
+                  // 表示将 done 字段置为 true
+                  ifstar: false
+                },
+                success: function(res) {
+                console.log(res)
+                }
+              })
+        }else{
+            wx.cloud.database().collection('logs').doc(user_id).update({
+                // data 传入需要局部更新的数据
+                data: {
+                  // 表示将 done 字段置为 true
+                  ifstar: true
+                },
+                success: function(res) {
+                console.log(res)
+                }
+              })
+        }
+        
     })
     }
 })
