96f65f29af019792594330580e10ef7c6f80dd19
Liaojixiu <772512568@qq.com>
2021-08-22 16:43:02 +0800

    日志窗口
    日志窗口

--- pages/day/day.js
+++ pages/day/day.js
@@ -1,66 +1,69 @@
-// pages/day/day.js
+// pages/write/write.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
+    data:  {
+      weidu:null,
+      quanbu:null, 
+      shuju:[],
+      listData:[
+        {"id":"4545","code":"张三","text":"今天我吃了很多大学，学习了核对的知识，深度学习真难啊","type":true},
+        {"id":"45","code":"张三","text":"wewe今天我吃了很多大学，学习了核对的知识，深度学习真难啊","type":true},
+        {"id":"445","code":"张三","text":"未读今天我吃了很多大学，学习了核对的知识，深度学习真难啊","type":true},
+        {"id":"545","code":"张三","text":"请问今天我吃了很多大学，学习了核对的知识，深度学习真难啊","type":true},
+        {"id":"230","code":"李四","text":"text2","type":true},
+        {"id":"76","code":"王五","text":"text3","type":true},
+        {"id":"560","code":"小王","text":"text4","type":true},
+        {"id":"79","code":"小徐","text":"text5","type":true},
+        {"id":"78","code":"小廖","text":"text6","type":false},
+        {"id":"63","code":"乐乐","text":"text7","type":false}
+      ]},
+    
     onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
-})
+      console.log('onLoad') 
+      var that = this
+      that.setData({
+        weidu:options.nameData,
+        quanbu:options.ageData
+      }) },
+  
+      detail: function(param){
+        var that = this
+        wx.navigateTo({
+            url: '/pages/check/check?id=' + that.data.listData.id
+                })
+      },
+      inputgetname:function(e){
+        this.setData({
+          getname:e.detail.value
+        })
+      },
+      searchDataNameFn:function(){
+        wx.cloud.callFunction({
+          name:'useroption',
+          data:{
+            option:'get',
+            getname:this.data.getname
+          },
+          success: res => {
+            this.setData({
+              array:res.result.data
+            })
+            wx.showToast({
+              title:"我好累",
+            })
+            console.log(res.result.data)
+          },
+          fail:err => {
+            console.log(err)
+          }
+        })
+      },
+  
+    onReady: function () {},
+    onShow: function () {},
+    onHide: function () {},
+    onUnload: function () {},
+    onPullDownRefresh: function () {},
+    onReachBottom: function () {},
+    onShareAppMessage: function () {}
+  })
