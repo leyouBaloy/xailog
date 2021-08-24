b6d3c6e7157beb02aed3e91ba314d972de51413c
leyouBaloy <71641224+leyouBaloy@users.noreply.github.com>
2021-08-25 00:27:17 +0800

    帮朔朔查到了数据

--- pages/bgslpm/bgslpm.js
+++ pages/bgslpm/bgslpm.js
@@ -3,11 +3,15 @@ Page({
     data: {
         show: false,
         currentDate: new Date().getTime(),
-        date: null
+        date: null,
+        openids:[],
+        cnt_by_month:[],
+        cnt_by_day:[],
       },
 
       onLoad() {
-        this.getSDate()
+        this.getSDate(),
+        this.stat()
       },
 
       getSDate(){
@@ -34,4 +38,56 @@ Page({
         this.getSDate();
         this.onClose();
       },
+
+      stat(){
+        var that = this;
+        let currentDate = new Date();
+        let currentDateStamp = currentDate.getTime();
+        let currentYear = currentDate.getFullYear();
+        let currentMonth = currentDate.getMonth();
+        let currentDay = currentDate.getDate();
+        let openids = new Set();
+        let cnt_by_month = [];
+        let cnt_by_day = [];
+        let tmp_cnt_by_month = 0;
+        let tmp_cnt_by_day = 0;
+        wx.cloud.database().collection("logs")
+        .where({is_delete:0})
+        .get()
+        .then(res => {
+          console.log("查询logs成功", res)
+          for (var val of res.data){
+            openids.add(val._openid);
+          };
+          console.log("查到的openids",openids);//找到openids，放进集合里
+          that.setData({openids:openids});//数据库暂时没有name，用openid代替
+          for (var openid of openids){
+            for (var data of res.data){
+              if(openid==data._openid){
+                let _date = new Date(val.time);
+                let _year = _date.getFullYear();
+                let _month = _date.getMonth();
+                let _day = _date.getDate();
+                if (_year==currentYear && _month==currentMonth){
+                  tmp_cnt_by_month += 1;
+                }
+                if (_year==currentYear && _month==currentMonth && _date==currentDay){
+                  tmp_cnt_by_day += 1;
+                }
+              }
+            }
+            cnt_by_month.push(tmp_cnt_by_month);
+            tmp_cnt_by_month=0;
+            cnt_by_day.push(tmp_cnt_by_day);
+            tmp_cnt_by_day=0;
+          };
+          console.log("cnt_by_month的值", cnt_by_month);
+          that.setData({cnt_by_month:cnt_by_month});
+          console.log("cnt_by_day的值", cnt_by_day);
+          that.setData({cnt_by_day:cnt_by_day});
+        })
+        .catch(err => {
+          console.log("查询logs失败", err)
+        })
+      }
 })
