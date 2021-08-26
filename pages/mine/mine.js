8cdebdcefb120ead48ad01c586469dfb8f1fa77f
Sure-Plus <xfreell@163.com>
2021-08-26 17:10:10 +0800

    添加更新头像功能

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,5 +1,7 @@
 // pages/mine/mine.js
 import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+
 Page({
 
     /**
@@ -32,6 +34,35 @@ Page({
       })
         
     },
+    changeAvatar() {
+      Dialog.confirm({
+        message: '是否更新头像为当前微信头像\n(需重新授权)',
+      })
+        .then(() => {
+          wx.getUserProfile({
+            desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (res) => {
+              wx.setStorageSync('userInfo', res.userInfo)
+            }
+          })
+          wx.cloud.database().collection('mine').doc(this.data.userInfo._id)
+          .update({
+            data: {
+              avatar: wx.getStorageSync('userInfo').avatarUrl
+            }
+          })
+          .then(res=> {
+            var tem = this.data.userInfo
+            tem.avatar = wx.getStorageSync('userInfo').avatarUrl
+            this.setData({
+              userInfo:tem
+            })
+          })
+        })
+        .catch(() => {
+          console.log("关闭弹窗")
+        });
+    },
     openConfirm: function () {
       this.setData({
           dialogShow: true
@@ -47,16 +78,12 @@ Page({
             name:e.detail.value.input
           }})
         .then(res=> {
-            wx.cloud.database().collection('mine').where({
-              _openid:this.data.openid
-            })
-            .get()
-            .then(res=>{
-              this.setData({
-                userInfo:res.data[0],
-                dialogShow: false
-              })
-            })
+          var tem = this.data.userInfo
+          tem.name = e.detail.value.input
+          this.setData({
+            userInfo:tem,
+            dialogShow: false
+          })
         })
       }
       else{
