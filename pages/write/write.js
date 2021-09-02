4af907d195310a09e91907fe3e146a72550e801e
leyouBaloy <71641224+leyouBaloy@users.noreply.github.com>
2021-09-02 19:01:12 +0800

    现在已经可以上传任何附件

--- pages/write/write.js
+++ pages/write/write.js
@@ -10,7 +10,7 @@ Page({
     //日志内容
     content: "",
     //附件
-    accept:'image',//上传内容格式，video
+    accept:'all',//上传内容格式，video
     fileList: [], //上传文件临时存储
     fileIDs: [],//文件上传后获取云数据中的位置
     reject: [],//用于uploadfilepromise的promise判断是否上传图片成功
@@ -78,19 +78,22 @@ Delete(event) {
     }
   })
 },
+
 // 提交
 submit() {
-  // 先上传图片
+  // 先上传附件
   var that = this;
   let fileList= this.data.fileList;
   if (that.data.date.length==0 || that.data.content==0) {
     wx.showToast({ title: '请完善内容', icon: 'none' });
   } else {
-    const uploadTasks = fileList.map(item => this.uploadFilePromise(item.name,item.url,that));
-    // console.log("submit里的uploadTaks内容",uploadTasks);
+    const uploadTasks = fileList.map(item => this.uploadFilePromise(item.name,item.url));
+    console.log("submit里的uploadTaks内容",uploadTasks);
     Promise.all(uploadTasks)
       .then(data => {
         wx.showToast({ title: '上传成功', icon: 'none' });
+        const _fileIDs = data.map(item => (item.fileID));
+        this.setData({ cloudPath: data, fileIDs: _fileIDs });
         //再提交数据到数据库
     wx.cloud.database().collection("logs")
     .add({
@@ -120,30 +123,14 @@ submit() {
         console.log(e);
       });
   }
-},
 
-uploadFilePromise(fileName, url, that) {
-  wx.cloud.uploadFile({
-    cloudPath: "appendix/"+fileName,
-    filePath: url,
-    success: res => {
-      // console.log("uploadfile成功的res:", res);
-      // resFileID = res.fileID;
-      let _fileIDs = that.data.fileIDs;
-      _fileIDs.push(res.fileID);
-      that.setData({fileIDs: _fileIDs});
-      console.log("fileIDs的值", that.data.fileIDs);
-      // console.log("临时fileIDs的值", _fileIDs);
-      that.data.setData({reject:Promise.resolve("成功")})
-
-    },
-    fail(err) {
-      console.log("上传失败", res);
-      that.data.setData({reject:Promise.reject("失败")})
-    }
-  });
-  return that.data.reject
 },
+uploadFilePromise(fileName, url) {
+  return wx.cloud.uploadFile({
+    cloudPath: fileName,
+    filePath: url
+  })},
+
 // 开关
 switchOnChange({ detail }) {
   // 需要手动对 checked 状态进行更新
