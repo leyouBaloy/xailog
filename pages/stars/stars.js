bb8eef6ff41190fc1d5faa442e79c0a785bc8fb6
leyouBaloy <71641224+leyouBaloy@users.noreply.github.com>
2021-09-07 17:56:50 +0800

    小昕星统计完成

--- pages/stars/stars.js
+++ pages/stars/stars.js
@@ -4,11 +4,15 @@ Page({
     data: {
         show: false,
         currentDate: new Date().getTime(),
-        date: null
+        date: null,
+        // star_logs:null,
+        mineWithStar:[]
     },
 
     onLoad: function (options) {
         this.getSDate();
+        this.stat();
+        // this.get_star_logs();
     },
 
     getSDate(){
@@ -31,6 +35,53 @@ Page({
         });
         this.getSDate();
         this.onClose();
-      }
+      },
+
+      async stat(){
+        wx.cloud.database().collection("mine").get().then(ress => {
+        let that = this;
+        let currentDate = new Date();
+        let currentMonth = currentDate.getMonth();
+        // let currentDay = currentDate.getDate();
+
+        const dbc = wx.cloud.database().command;
+
+        wx.cloud.database().collection("logs")
+            .where(dbc.or({is_delete:false},{ifstar:true}))
+            .get().then(res=>{
+              // that.setData({star_logs:res.data});
+              console.log("查询到的logs",res.data);
+              console.log("查询到的mine",ress.data);
+              for(var log of res.data){
+                for(var mine of ress.data){
+                  if(!mine.allStars){
+                    mine.allStars = 0;
+                    mine.monthStars = 0;
+                  };
+                  if(log._openid==mine._openid){
+                    let _date = new Date(log.time)
+                  let _month = _date.getMonth();
+                  
+                  mine.allStars += 1;
+                  if(_month == currentMonth){
+                    mine.monthStars +=1;
+                  }
+                  }
+                }
+              }
+              console.log("统计后的结果",ress.data);
+              that.setData({mineWithStar:ress.data})
+
+
+              
+            })
+          })
+    },
+
+
+
+
+
 
 })
+
