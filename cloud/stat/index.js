e13e054ff7e5639fb24d9d55d486e9a1f27bdfbf
leyouBaloy <1942956063@qq.com>
2021-12-21 10:40:53 +0800

    报告数量排名

--- cloud/stat/index.js
+++ cloud/stat/index.js
@@ -6,16 +6,25 @@ const db = cloud.database()
 const _ = db.command
 const $ = db.command.aggregate
 
+var newdate = new Date()
+var preDayStamp = new Date(newdate.getFullYear(),newdate.getMonth(),newdate.getDate()-1).getTime()
+var preMonthStamp = new Date(newdate.getFullYear(),newdate.getMonth()-1,newdate.getDate()).getTime()
+
 // 云函数入口函数
 exports.main = async (event, context) => {
     var data = await db.collection("logs").aggregate()
     .match({
         is_delete:false,
-        time:_.gte((new Date()).getTime()-24*60*60*1000*30)
+        time:_.gte(preMonthStamp)
     })
     .group({
         _id: '$_openid',
-        count:$.sum(1),
+        countByMonth:$.sum(1),
+        countByDay:$.sum($.cond({
+            if: $.gt(['$time',preDayStamp]),
+            then:1,
+            else:0
+        })),
         stars:$.sum($.cond({
             if: $.eq(['$ifstar',true]),
             then:1,
@@ -32,14 +41,15 @@ exports.main = async (event, context) => {
             as: "name"
         })
         .sort({
-            count:-1
+            countByMonth:-1
         })
         .end()
     var res = data.list.map((item)=>{
         return {
             _openid:item._id,
             name:item.name[0].name,
-            count:item.count,
+            countByMonth:item.countByMonth,
+            countByDay:item.countByDay,
             stars:item.stars
         }
     })
--- pages/bgslpm/bgslpm.js
+++ pages/bgslpm/bgslpm.js
@@ -3,24 +3,13 @@
 Page({
     data: {
         show: false,
-        currentDate: new Date().getTime(),
-        date: null,
-        openids:[],
-        cnt_by_month:[],
-        cnt_by_day:[],
-        cnt_table:[],//把列表列转行，便于渲染
-        names: [],
+        data:[],
+        date:null,
       },
 
       onLoad() {
-        this.getSDate();
+        this.getSDate()
         this.stat();
-      },
-
-      getSDate(){
-        var tem = new Date(this.data.currentDate);
-        this.setData({ date: String(tem.getFullYear())+'年'+String(tem.getMonth()+1)+'月' });
-        // console.log(this.data.date)
       },
 
       showPopup() {
@@ -42,93 +31,25 @@ Page({
         this.onClose();
       },
 
+      getSDate(){
+        var newdate = new Date()
+        var pre = new Date(newdate.getFullYear(),newdate.getMonth()-1,newdate.getDate())
+        this.setData({ 
+          date: String(pre.getMonth()+1)+'月'+String(pre.getDate())+"日-"+
+        String(newdate.getMonth()+1)+'月'+String(newdate.getDate())+"日"
+       });
+        // console.log(this.data.date)
+      },
+
       stat(){
-        wx.cloud.database().collection("mine").get().then(ress => {
-          console.log("查询mine成功", ress)
-        
-        var that = this;
-        let currentDate = new Date();
-        let currentDateStamp = currentDate.getTime();
-        let currentYear = currentDate.getFullYear();
-        let currentMonth = currentDate.getMonth();
-        let currentDay = currentDate.getDate();
-        let openids = new Set();
-        let cnt_by_month = [];
-        let cnt_by_day = [];
-        let tmp_cnt_by_month = 0;
-        let tmp_cnt_by_day = 0;
-        let _names = [];
-        // wx.cloud.database().collection("logs")
-        // .where({is_delete:false})
-        // .get()
-        wx.cloud.callFunction({name: "query"})
-        .then(res => {
-          console.log("查询logs成功", res.result)
-          for (var val of res.result){
-            openids.add(val._openid);
-          };
-          openids = [...openids]; //转成数组
-          console.log("查到的openids",openids);//找到openids
-          that.setData({openids:openids});
-
-          // 一个巨大的循环openid
-          for (var openid of openids){
-            // 再统计cnt_by_month和cnt_by_day
-            for (var data of res.result){
-              if(openid==data._openid){
-                let _date = new Date(data.time);
-                let _year = _date.getFullYear();
-                let _month = _date.getMonth();
-                let _day = _date.getDate();
-                if (_year==currentYear && _month==currentMonth){
-                  tmp_cnt_by_month += 1;
-                  // console.log("currentMonth的值", currentMonth,"_month的值", _month);
-                  if (_day == currentDay) {
-                    // console.log("currentDay的值", currentDay,"_day的值", _day);
-                    tmp_cnt_by_day += 1;
-                  }
-                }
-              }
-            }
-            cnt_by_month.push(tmp_cnt_by_month);
-            tmp_cnt_by_month=0;
-            cnt_by_day.push(tmp_cnt_by_day);
-            tmp_cnt_by_day=0;
-            for (var k of ress.data){
-              if (k._openid==openid){
-                _names.push(k.name)
-              }
-            }
-          };
-          console.log("cnt_by_month的值", cnt_by_month);
-          that.setData({cnt_by_month:cnt_by_month});
-          console.log("cnt_by_day的值", cnt_by_day);
-          that.setData({cnt_by_day:cnt_by_day});
-          console.log("_names的值", _names);
-          that.setData({names:_names})
-
-          
-
-
-
-          // 转置,由三个列表获得一个cnt_table嵌套列表
-            let inner_arr = [];
-            let outer_arr = [];
-            // let that = this
-            // console.log("this.data", this.data)
-            for(var j=0;j<this.data.openids.length;j++){
-              // console.log("this.data.openid[j]的值", this.data.openids[j]);
-              outer_arr.push([this.data.names[j], this.data.cnt_by_month[j], this.data.cnt_by_day[j],this.data.openids[j]]);
-            }
-            this.setData({cnt_table:outer_arr});
-            console.log("outer_arr/cnt_table的值", outer_arr);
-     
-      
-          })
-        .catch(err => {
-          console.log("查询logs失败", err)
-        });
-      })
+        wx.cloud.callFunction({name: "stat",})
+        .then(res=>{
+          console.log("调用云函数统计查询结果",res.result)
+          this.setData({data:res.result})
+        })
+        .catch(res=>{
+          console.log("云函数调用失败",res)
+        })
         
         },
 
